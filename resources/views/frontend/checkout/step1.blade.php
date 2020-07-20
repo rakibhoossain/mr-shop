7ddf1b0b3a9826dd11dfbada8321570d597a5add
Rakib Hossain <rakibhoossain@gmail.com>
2020-07-21 01:07:36 +0600

    show old value - validate

--- resources/views/frontend/checkout/step1.blade.php
+++ resources/views/frontend/checkout/step1.blade.php
@@ -10,36 +10,36 @@
       </div>
       <div class="row">
         <div class="form-group col-md-6">
-          <label for="firstname" class="form-label">First Name</label>
-          <input id="firstname" type="text" name="invoice[first_name]" placeholder="Enter you first name" class="form-control @error('invoice.first_name') is-invalid @enderror " value="{{{ (isset($invoice['first_name'])) ? $invoice['first_name'] : '' }}}">
+          <label for="firstname" class="form-label">First Name<span class="text-danger" title="This field is required">*</span></label>
+          <input id="firstname" type="text" name="invoice[first_name]" placeholder="Enter you first name" class="form-control @error('invoice.first_name') is-invalid @enderror " value="{{{ (isset($invoice['first_name'])) ? $invoice['first_name'] : old('invoice.first_name') }}}">
         </div>
         <div class="form-group col-md-6">
-          <label for="lastname" class="form-label">Last Name</label>
-          <input id="lastname" type="text" name="invoice[last_name]" placeholder="Enter your last name" class="form-control @error('invoice.last_name') is-invalid @enderror " value="{{{ (isset($invoice['last_name'])) ? $invoice['last_name'] : '' }}}">
+          <label for="lastname" class="form-label">Last Name<span class="text-danger" title="This field is required">*</span></label>
+          <input id="lastname" type="text" name="invoice[last_name]" placeholder="Enter your last name" class="form-control @error('invoice.last_name') is-invalid @enderror " value="{{{ (isset($invoice['last_name'])) ? $invoice['last_name'] : old('invoice.last_name') }}}">
         </div>
         <div class="form-group col-md-6">
-          <label for="phone-number" class="form-label">Phone Number</label>
-          <input id="phone-number" type="tel" name="invoice[phone_number]" placeholder="Your phone number" class="form-control @error('invoice.phone_number') is-invalid @enderror " value="{{{ (isset($invoice['phone_number'])) ? $invoice['phone_number'] : '' }}}">
+          <label for="phone-number" class="form-label">Phone Number<span class="text-danger" title="This field is required">*</span></label>
+          <input id="phone-number" type="tel" name="invoice[phone_number]" placeholder="Your phone number" class="form-control @error('invoice.phone_number') is-invalid @enderror " value="{{{ (isset($invoice['phone_number'])) ? $invoice['phone_number'] : old('invoice.phone_number') }}}">
         </div>
         <div class="form-group col-md-6">
           <label for="alternative-phone-number" class="form-label">Alternative Phone Number</label>
-          <input id="alternative-phone-number" type="tel" name="invoice[alternative_number]" placeholder="Your alternative phone number" class="form-control @error('invoice.alternative_number') is-invalid @enderror " value="{{{ (isset($invoice['alternative_number'])) ? $invoice['alternative_number'] : '' }}}">
+          <input id="alternative-phone-number" type="tel" name="invoice[alternative_number]" placeholder="Your alternative phone number" class="form-control @error('invoice.alternative_number') is-invalid @enderror " value="{{{ (isset($invoice['alternative_number'])) ? $invoice['alternative_number'] : old('invoice.alternative_number') }}}">
         </div>
         <div class="form-group col-md-4">
-          <label for="country" class="form-label">Country</label>
-          <input id="country" type="text" name="invoice[country]" placeholder="Your country" class="form-control @error('invoice.country') is-invalid @enderror " value="{{{ (isset($invoice['country'])) ? $invoice['country'] : 'Bangladesh' }}}">
+          <label for="country" class="form-label">Country<span class="text-danger" title="This field is required">*</span></label>
+          <input id="country" type="text" name="invoice[country]" placeholder="Your country" class="form-control @error('invoice.country') is-invalid @enderror " value="{{{ (isset($invoice['country'])) ? $invoice['country'] : old('invoice.country', 'Bangladesh') }}}">
         </div>
         <div class="form-group col-md-4">
-          <label for="city" class="form-label">City</label>
-          <input id="city" type="text" name="invoice[city]" placeholder="Your city" class="form-control @error('invoice.city') is-invalid @enderror " value="{{{ (isset($invoice['city'])) ? $invoice['city'] : '' }}}">
+          <label for="city" class="form-label">City<span class="text-danger" title="This field is required">*</span></label>
+          <input id="city" type="text" name="invoice[city]" placeholder="Your city" class="form-control @error('invoice.city') is-invalid @enderror " value="{{{ (isset($invoice['city'])) ? $invoice['city'] : old('invoice.city') }}}">
         </div>
         <div class="form-group col-md-4">
-          <label for="post_code" class="form-label">Post code</label>
-          <input id="post_code" type="text" name="invoice[post_code]" placeholder="Your post code" class="form-control @error('invoice.post_code') is-invalid @enderror " value="{{{ (isset($invoice['post_code'])) ? $invoice['post_code'] : '' }}}">
+          <label for="post_code" class="form-label">Post code<span class="text-danger" title="This field is required">*</span></label>
+          <input id="post_code" type="text" name="invoice[post_code]" placeholder="Your post code" class="form-control @error('invoice.post_code') is-invalid @enderror " value="{{{ (isset($invoice['post_code'])) ? $invoice['post_code'] : old('invoice.post_code') }}}">
         </div>
         <div class="form-group col-md-12">
-          <label for="invoice_address" class="form-label">Address</label>
-          <textarea id="invoice_address" placeholder="Your Address" class="form-control @error('invoice.address') is-invalid @enderror " name="invoice[address]" >{{{ (isset($invoice['address'])) ? $invoice['address'] : '' }}}</textarea>
+          <label for="invoice_address" class="form-label">Address<span class="text-danger" title="This field is required">*</span></label>
+          <textarea id="invoice_address" placeholder="Your Address" class="form-control @error('invoice.address') is-invalid @enderror " name="invoice[address]" >{{{ (isset($invoice['address'])) ? $invoice['address'] : old('invoice.address') }}}</textarea>
         </div>
 
         <div class="form-group col-12 mt-3 ml-3">
@@ -55,37 +55,37 @@
         </div>
         <div class="row">
           <div class="form-group col-md-6">
-            <label for="shipping_firstname" class="form-label">First Name</label>
-            <input id="shipping_firstname" type="text" name="shipping[first_name]" placeholder="Enter you first name" class="form-control @error('shipping.first_name') is-invalid @enderror " value="{{{ (isset($shipping['first_name'])) ? $shipping['first_name'] : '' }}}">
+            <label for="shipping_firstname" class="form-label">First Name<span class="text-danger" title="This field is required">*</span></label>
+            <input id="shipping_firstname" type="text" name="shipping[first_name]" placeholder="Enter you first name" class="form-control @error('shipping.first_name') is-invalid @enderror " value="{{{ (isset($shipping['first_name'])) ? $shipping['first_name'] : old('shipping.first_name') }}}">
           </div>
           <div class="form-group col-md-6">
-            <label for="shipping_lastname" class="form-label">Last Name</label>
-            <input id="shipping_lastname" type="text" name="shipping[last_name]" placeholder="Enter your last name" class="form-control @error('shipping.last_name') is-invalid @enderror " value="{{{ (isset($shipping['last_name'])) ? $shipping['last_name'] : '' }}}">
+            <label for="shipping_lastname" class="form-label">Last Name<span class="text-danger" title="This field is required">*</span></label>
+            <input id="shipping_lastname" type="text" name="shipping[last_name]" placeholder="Enter your last name" class="form-control @error('shipping.last_name') is-invalid @enderror " value="{{{ (isset($shipping['last_name'])) ? $shipping['last_name'] : old('shipping.last_name') }}}">
           </div>
           <div class="form-group col-md-6">
-            <label for="shipping_phone-number" class="form-label">Phone Number</label>
-            <input id="shipping_phone-number" type="tel" name="shipping[phone_number]" placeholder="Your phone number" class="form-control @error('shipping.phone_number') is-invalid @enderror " value="{{{ (isset($shipping['phone_number'])) ? $shipping['phone_number'] : '' }}}">
+            <label for="shipping_phone-number" class="form-label">Phone Number<span class="text-danger" title="This field is required">*</span></label>
+            <input id="shipping_phone-number" type="tel" name="shipping[phone_number]" placeholder="Your phone number" class="form-control @error('shipping.phone_number') is-invalid @enderror " value="{{{ (isset($shipping['phone_number'])) ? $shipping['phone_number'] : old('shipping.phone_number') }}}">
           </div>
 
           <div class="form-group col-md-6">
             <label for="shipping_alternative-number" class="form-label">Alternative Phone Number</label>
-            <input id="shipping_alternative-number" type="tel" name="shipping[alternative_number]" placeholder="Your alternative phone number" class="form-control @error('shipping.alternative_number') is-invalid @enderror " value="{{{ (isset($shipping['alternative_number'])) ? $shipping['alternative_number'] : '' }}}">
+            <input id="shipping_alternative-number" type="tel" name="shipping[alternative_number]" placeholder="Your alternative phone number" class="form-control @error('shipping.alternative_number') is-invalid @enderror " value="{{{ (isset($shipping['alternative_number'])) ? $shipping['alternative_number'] : old('shipping.alternative_number') }}}">
           </div>
           <div class="form-group col-md-4">
-            <label for="shipping_country" class="form-label">Country</label>
-            <input id="shipping_country" type="text" name="shipping[country]" placeholder="Your country" class="form-control @error('shipping.country') is-invalid @enderror " value="Bangladesh" value="{{{ (isset($shipping['country'])) ? $shipping['country'] : '' }}}">
+            <label for="shipping_country" class="form-label">Country<span class="text-danger" title="This field is required">*</span></label>
+            <input id="shipping_country" type="text" name="shipping[country]" placeholder="Your country" class="form-control @error('shipping.country') is-invalid @enderror " value="Bangladesh" value="{{{ (isset($shipping['country'])) ? $shipping['country'] : old('shipping.country', 'Bangladesh') }}}">
           </div>
           <div class="form-group col-md-4">
-            <label for="shipping_city" class="form-label">City</label>
-            <input id="shipping_city" type="text" name="shipping[city]" placeholder="Your city" class="form-control @error('shipping.city') is-invalid @enderror " value="{{{ (isset($shipping['city'])) ? $shipping['city'] : '' }}}">
+            <label for="shipping_city" class="form-label">City<span class="text-danger" title="This field is required">*</span></label>
+            <input id="shipping_city" type="text" name="shipping[city]" placeholder="Your city" class="form-control @error('shipping.city') is-invalid @enderror " value="{{{ (isset($shipping['city'])) ? $shipping['city'] : old('shipping.city') }}}">
           </div>
           <div class="form-group col-md-4">
-            <label for="shipping_post_code" class="form-label">Post code</label>
-            <input id="shipping_post_code" type="text" name="shipping[post_code]" placeholder="Your post code" class="form-control @error('shipping.post_code') is-invalid @enderror " value="{{{ (isset($shipping['post_code'])) ? $shipping['post_code'] : '' }}}">
+            <label for="shipping_post_code" class="form-label">Post code<span class="text-danger" title="This field is required">*</span></label>
+            <input id="shipping_post_code" type="text" name="shipping[post_code]" placeholder="Your post code" class="form-control @error('shipping.post_code') is-invalid @enderror " value="{{{ (isset($shipping['post_code'])) ? $shipping['post_code'] : old('shipping.post_code') }}}">
           </div>
           <div class="form-group col-md-12">
-            <label for="shipping_address" class="form-label">Address</label>
-            <textarea id="shipping_address" placeholder="Your Address" class="form-control @error('shipping.address') is-invalid @enderror " name="shipping[address]" >{{{ (isset($shipping['address'])) ? $shipping['address'] : '' }}}</textarea>
+            <label for="shipping_address" class="form-label">Address<span class="text-danger" title="This field is required">*</span></label>
+            <textarea id="shipping_address" placeholder="Your Address" class="form-control @error('shipping.address') is-invalid @enderror " name="shipping[address]" >{{{ (isset($shipping['address'])) ? $shipping['address'] : old('shipping.address') }}}</textarea>
           </div>
         </div>
       </div>
